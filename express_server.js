f21ce5232c10d8c8ecaa19c1e8f65f84f6551c8c
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-27 21:00:10 +0000

    add ejs to express_server.js and added href in urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -3,16 +3,26 @@ const express =require("express");
 const app = express(); 
 const PORT = 8080; 
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
-
-app.get("/hello", (req, res) => {
-  // res.send("check yo!");
-  // res.json(urlDatabase);
-  res.send("<html><body>hello <b>world </b></body></html>\n")
+app.get ("/urls", (req,res) =>{ 
+  const templateVars = {urls : urlDatabase};
+  res.render("urls_index", templateVars);
 })
+
+app.get("/hello", (req,res) =>{
+  const templateVars = {greeting: ""}; 
+  res.render("urls_index" , templateVars);
+})
+// app.get("/hello", (req, res) => {
+//   // res.send("check yo!");
+//   // res.json(urlDatabase);
+//   res.send("<html><body>hello <b>world </b></body></html>\n")
+// })
 app.listen(PORT, ()=>{
   console.log(`example app is listening on port ${PORT}`);
 }) 
