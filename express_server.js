5f5f54dedb8975792c8cf1f17183dc448c24ea53
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-28 02:20:30 +0000

    added urls_new

--- express_server.js
+++ express_server.js
@@ -1,27 +1,60 @@
 const { response } = require("express");
 const express =require("express"); 
 const app = express(); 
+const bodyParser = require("body-parser") ; 
 const PORT = 8080; 
 
+
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  return (Math.random()+1).toString(36).substring(6);
+}
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls" , (req,res)=>{
+  console.log(req.body); 
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect("/urls");
+;})
+
 app.get ("/urls", (req,res) =>{ 
   const templateVars = {urls : urlDatabase};
   res.render("urls_index", templateVars);
 })
 
+app.get("/u/:shortURL", (req,res) => {
+  console.log(req.params); 
+  const sURL = req.params.shortURL; 
+  const lURL = urlDatabase[sURL];
+  if (lURL){
+  return res.redirect(lURL);
+  } 
+  return res.send("Did not find this short URL's corresponding long URL")
+})
+
 app.get("/urls/:shortURL", (req,res) => {
   console.log(req.params); 
-  const templateVars={shortURL:req.params.shortURL, longURL:urlDatabase[req.params.shortURL]}
-  if (urlDatabase[req.params.shortURL]){
+  const sURL = req.params.shortURL; 
+  const lURL = urlDatabase[sURL];
+  const templateVars={shortURL:sURL, longURL:lURL}
+  if (lURL){
     return res.render("urls_show", templateVars); 
   } 
   return res.send("Did not find this Long URL")
 })
+
 app.listen(PORT, ()=>{
   console.log(`example app is listening on port ${PORT}`);
 }) 
